de5c935c5cc735e5a2c60d4610f800d88fa98fd3
Siddharth Kumar Shukla <cpcodebharat@gmail.com>
2026-01-21 19:37:20 +0530

    Fix formatting in create_vectorstore function

--- backend/rag/vectorstore.py
+++ backend/rag/vectorstore.py
@@ -1,9 +1,7 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
-
 def create_vectorstore(chunks):
     embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2"
+        model_name = "all-MiniLM-L6-v2"
     )
     return FAISS.from_texts(chunks, embeddings)
