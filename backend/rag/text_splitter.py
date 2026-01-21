da3b2cbb2fcea9804a70c3004db499449ebce5ab
Siddharth Kumar Shukla <cpcodebharat@gmail.com>
2026-01-21 19:37:01 +0530

    Fix formatting of parameters in split_text function

--- backend/rag/text_splitter.py
+++ backend/rag/text_splitter.py
@@ -1,8 +1,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 def split_text(text: str):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800,
-        chunk_overlap=150
+        chunk_size = 800,
+        chunk_overlap = 150
     )
     return splitter.split_text(text)
